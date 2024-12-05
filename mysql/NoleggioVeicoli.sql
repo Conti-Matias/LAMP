3fc2e6754846a00c33045b70a78e8669792d0e52
Conti-Matias <matimex06@gmail.com>
2024-12-05 08:59:25 +0000

    j

--- mysql/NoleggioVeicoli.sql
+++ mysql/NoleggioVeicoli.sql
@@ -7,7 +7,7 @@ CREATE TABLE Clienti (
     cognome VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     telefono VARCHAR(15),
-    data_registrazione DATE DEFAULT CURRENT_DATE
+    data_registrazione TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE Veicoli (
