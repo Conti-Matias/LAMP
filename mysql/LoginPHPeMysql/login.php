638ae50266cc61d874fc508e12eab2c9536884b4
Conti-Matias <matimex06@gmail.com>
2025-02-20 08:24:47 +0000

    f

--- mysql/LoginPHPeMysql/login.php
+++ mysql/LoginPHPeMysql/login.php
@@ -1,37 +1,49 @@
 <?php
-// Costanti per la connessione al database
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'db_user');
-define('DB_PASSWORD', 'db_pwd');
-define('DB_NAME', 'nome_database');
-$html_out = "";
-try {
-// Connessione al database
-$conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+// Includo il file delle funzioni
+include 'functions.php';
 
-// Verifica della connessione
-if (!$conn) {
-    //die("Connessione fallita: " . mysqli_connect_error());
-    $html_out = "Attenzione! Connessione al database fallita." . mysqli_connect_error();
-}
+// Avvio la sessione php per recuperare eventuali dati di sessione
+// ...
 
-$html_out = "Connessione al database riuscita.";
-// ... successivamente eseguire le query qui ...
+// Recupero eventuali dati dal form di login
+// Recupero eventuali dati inviati tramite query string
 
-// Chiusura della connessione
-mysqli_close($conn);
-} catch (Exception $e) {
-$html_out = "Attenzione! Si è verificata un'eccezione. " . $e->getMessage();
-}
+// Controllo se l'utente è registrato e se la password è corretta
+[$retval, $errmsg] = login($utente, $passw);
+
+// Creo il codice html da visualizzare a seconda dei valori di $from e $retval
+//if ...
+  // se from è valorizzato significa che l'utente ha tentato di accedere ad una pagina riservata
+  // senza essere autenticato, quindi visualizzo il messaggio di errore e il form di login
+  $htlm = "<p class='error'>$from_msg</p>";
+  $htlm .= $html_form;
+  // ...
+?>
+<?php
+//Form di login
+$html_form = <<<FORM
+<form action="$_SERVER[PHP_SELF]" method="post">
+  <label for="utente"> </label><input type="text" name="utente" placeholder="utente" required/><br />
+  <label for="password"> </label><input type="password" name="password" placeholder="Password" required/><br />
+  <input type="submit" value="Login" /><input type="reset" value="Cancel" />
+</form>
+FORM;
+
+// Creo il codice html da visualizzare a seconda dei valori di $from e $retval
+  $html_out = "<p class='error'>$errmsg</p>";
+  $html_out .= $html_form;
+  $html_out .= "Non hai un account? <a href='register.php'>Registrati adesso</a>.<br />";
+  $html_out .= "Hai dimenticato la password? <a href='pwd_reset.php'>Resetta la password</a>.<br />";
+  $html_out .= "<a href='index.php'>Torna alla Home Page</a>.<br />";
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <title>Login</title>
-  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-  <h2>Test della connessione al database</h2>
+  <h2>Pagina di login</h2>
   <?=$html_out?>
 </body>
 </html>
+
