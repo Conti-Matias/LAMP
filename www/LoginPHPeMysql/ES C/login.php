7b0b449de9a43294ea5ca1b7360c0b9153a4d995
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2025-04-09 13:18:29 +0200

    Update login.php

--- www/LoginPHPeMysql/ES C/login.php
+++ www/LoginPHPeMysql/ES C/login.php
@@ -1,57 +1,65 @@
-<?php 
+<?php
+// Includo il file delle funzioni
+include 'functions.php';
 
-require 'funzioni.php';
-
+// Avvio la sessione php per recuperare eventuali dati di sessione
 session_start();
 
-if (isset($_SESSION['username'])) {
-    $mess = 'Accesso gia effettuato';
+$msg = $_GET['error'] ?? '';
+
+if(isset($_SESSION['username'])) {
+    $msg = 'Login già effettuato';
 }
-else if ($_SESSION['REQUEST_METHOD'] == 'POST') {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+else if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+  try{
+    [$loginRetval, $loginRetmsg] = login($username, $password);
+    
+    $msg = $loginRetmsg;
+    
+    if($loginRetval) {
+        $_SESSION['username'] = $username; 
 
-    [$Accessoretval, $Accessomsg] = controllo_login($username, $password);
+        $link = 'Location: ';
+        $link .= $_POST['from'] != null ? $_POST['from'] : 'index.php';
 
-    $mess = $Accessomsg;
+        header($link);
+        die();
 
-    if ($loginRetval) {
-        $_SESSION['username'] = $username;
-        
-        $redirectPage = $_POST['from'] ?? 'index.php';
-        header("Location: $redirectPage");
-        exit;
     }
-
-    $collegamento= impostaCollegamento();
+  }catch(Exception $e){
+    $msg = 'Errore durante il login: '. $e->getMessage();
+  }
 }
 ?>
 
+<?php
+//Form di login
+$html_form = <<<FORM
+<form action="$_SERVER[PHP_SELF]" method="post">
+  <label for="nome"> </label><input type="text" name="username" placeholder="Nome utente" required/><br />
+  <label for="password"> </label><input type="password" name="password" placeholder="Password" required/><br />
+  <input type="submit" value="Login" /><input type="reset" value="Cancel" />
+  <input type="hidden" name="from" value="{$_GET['from']}" />
+  <p class='error'>$msg</p>
+</form>
+FORM;
 
+// Creo il codice html da visualizzare a seconda dei valori di $from e $retval
+  $html_out = "<p class='error'>$errmsg</p>";
+  $html_out .= $html_form;
+  $html_out .= "Non hai un account? <a href='register.php'>Registrati adesso</a>.<br />";
+  $html_out .= "Hai dimenticato la password? <a href='pwd_reset.php'>Resetta la password</a>.<br />";
+  $html_out .= "<a href='index.php'>Torna alla Home Page</a>.<br />";
+?>
 <!DOCTYPE html>
-<html lang="it">
+<html>
 <head>
-    <title>Login</title>
+  <title>Login</title>
 </head>
 <body>
-        <h2>Login</h2>
-
-        <?= $msg ?>
-
-        <form action="<?= $_SERVER['PHP_SELF']; ?>" method="POST">
-            
-            <input type="text" name="username" id="username" placeholder="Username" pattern=".{3,}" required title="Minimo 3 lettere">
-            <br>
-            
-            <input type="password" name="password" id="password" placeholder="Password" pattern=".{3,}" required title="Minimo 3 lettere">
-            <br>
-            <input type="submit" value="Login" id="login-button">
-
-            <input type="hidden" name="from" value="<?= $_GET['from'] ?? '' ?>"> 
-        </form>
-            <?= $links ?>
+  <h2>Pagina di login</h2>
+  <?=$html_out?>
 </body>
 </html>
-
-
-    
