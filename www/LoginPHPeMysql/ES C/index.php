2ddb14a2043616379c0663aa9a8776429126232f
Conti-Matias <113691220+Conti-Matias@users.noreply.github.com>
2025-04-09 13:18:13 +0200

    Update index.php

--- www/LoginPHPeMysql/ES C/index.php
+++ www/LoginPHPeMysql/ES C/index.php
@@ -1,29 +1,26 @@
 <?php
-session_start(); // Avvio la sessione PHP
+// Avvio la sessione php per recuperare eventuali dati di sessione
+session_start();
 
 // Verifica se l'utente è autenticato
 $utente = $_SESSION['username'] ?? 'Ospite';
 
-// Creazione dei link in funzione dell'utente Ospite o Autenticato
-if ($utente === 'Ospite') {
-    $html_link = '<a href="login.php"><h2>Accedi</h2></a>';
-} else {
-    $html_link = '<a href="protected_page.php"><h2>riservata</h2></a> <a href="logout.php"><h2>Esci</h2></a>';
-}
+// Creazione dei link in funzione dell'utente Ospite o Autenticato 
+// ...
+$html_link .= '<a href="login.php">Pagina Login</a>';
+$html_link2 .= '<a href="riservata.php">Pagina Riservata</a>';
+ 
 ?>
-
 <!DOCTYPE html>
-<html lang="it">
+<html>
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
 </head>
 <body>
-  <h2>ES05 - Login con PHP e MySQL</h2>
-  <p>Benvenuto <?= $utente ?>. Questa è la home page del sito.<br />
-
+  <h2>ES05 - Login con PHP e MySql</h2>
+  <p>Benvenuto <?=$utente?>. Questa è la home page del sito.<br />
+  <?=$html_link?><br/>
+  <?=$html_link2?>
   <!-- Link -->
-  <p><?= $html_link ?></p>
 </body>
 </html>
